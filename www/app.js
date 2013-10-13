9a3d0fff0e45b00ac297a4ef9907e87fe03cd119
Hervormde Gemeente Katwijk aan Zee <admin@hervormdegemeentekatwijk.nl>
2013-10-13 11:29:33 -0400

    styling

--- www/app.js
+++ www/app.js
@@ -106,15 +106,30 @@ controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
   $scope.sermonsByDay = {};
   var sermon;
+  var months = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"];
+  var days = ["Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"];
+  var dayparts = ["Ochtend", "Middag", "Avond"];
+
   for (var i = 0; i < sermons.length; i++) {
     sermon = sermons[i];
+    var d = new Date(sermon.date);
+    var dp = parseInt(sermon.time.substr(0, 2), 10);
+    if (dp < 12) {
+      dp = dayparts[0];
+    }
+    else if (dp < 18) {
+      dp = dayparts[1];
+    }
+    else {
+      dp = dayparts[2];
+    }
 
     $scope.sermonsByDay[sermon.date] = $scope.sermonsByDay[sermon.date] || {
-      date: sermon.date,
+      date: days[d.getDay()] + ' ' + d.getDate() + ' ' + months[d.getMonth()],
       dayparts: {}
     };
     $scope.sermonsByDay[sermon.date].dayparts[sermon.time] = $scope.sermonsByDay[sermon.date].dayparts[sermon.time] || {
-      daypart: sermon.time,
+      daypart: dp,
       sermons: {}
     };
 
